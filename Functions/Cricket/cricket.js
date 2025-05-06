ac9ed43cf6fb85acc54327c488fdf8d3ab138289
kshitiznjr11 <kshitiznjr1@gmail.com>
2025-05-06 14:24:39 +0545

    functions

--- Functions/Cricket/cricket.js
+++ Functions/Cricket/cricket.js
@@ -10,3 +10,35 @@ function generateComputerChoice () {
         return 'Stump';
     }
 }
+
+function getResult(userMove, computerMove) {
+    if (userMove === 'Bat') {
+        if (computerMove === 'Ball') {
+            return 'You Won!';
+        } else if (computerMove === 'Bat') {
+            return `It's a tie!`;
+        } else if (computerMove === 'Stump') {
+            return 'Computer Won!';
+        }
+    } else if (userMove === 'Ball') {
+        if (computerMove === 'Ball') {
+            return `It's a tie!`;
+        } else if (computerMove === 'Bat') {
+            return 'Computer Won!';
+        } else if (computerMove === 'Stump') {
+            return 'You Won!';
+        }
+    } else {
+        if (computerMove === 'Ball') {
+            return 'Computer Won!';
+        } else if (computerMove === 'Bat') {
+            return 'You Won!';
+        } else if (computerMove === 'Stump') {
+            return `It's a tie!`;
+        }
+    }
+}
+
+function showResult(userMove, computerMove, result) {
+    alert(`You have choosen ${userMove}. Computer choice is ${computerMove}. ${result}`);
+}
