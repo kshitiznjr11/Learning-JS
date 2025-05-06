18343f2931a870964c71e5d6543f6dc724e8a74f
kshitiznjr11 <kshitiznjr1@gmail.com>
2025-05-06 13:50:44 +0545

    global variable

--- Functions/Cricket/cricket.js
+++ Functions/Cricket/cricket.js
@@ -1,12 +1,12 @@
-let computerChoice;
+
 function generateComputerChoice () {
     //This will generate random number between 0 and 1
     let randomNumber = Math.random() * 3;
     if (randomNumber > 0 && randomNumber <= 1) {
-        computerChoice = 'Bat';
+       return 'Bat';
     } else if (randomNumber > 1 && randomNumber <= 2) {
-        computerChoice = 'Ball';
+        return 'Ball';
     } else {
-        computerChoice = 'Stump';
+        return 'Stump';
     }
 }
