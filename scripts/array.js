dc9b4622fc39985c3b4293a285393c4e2b1154a2
kshitiznjr11 <kshitiznjr1@gmail.com>
2025-05-09 11:30:53 +0545

    todo app

---
+++ scripts/array.js
@@ -0,0 +1,17 @@
+// let numbers = [1,2,4,6,8,9,5,4];
+// console.log(numbers);
+// // console.log(numbers[2]);
+
+// let obj = {name: 'kshitiz'};
+// let doosra = [1, true, null, 'kshitiz'];
+// // console.log(doosra[3]);
+// // console.log(typeof doosra);
+
+// // console.log(Array.isArray(obj));
+
+let arr = [1,2,3];
+console.log(arr);
+arr.push(6);
+console.log(arr);
+arr.pop();
+console.log(arr);
